98bd4b60eb6716f82715a607c5b12983e91cdb22
eakylova <eakylova@beeline.kg>
2022-02-11 16:54:27 +0600

    added duration field to Question

--- src/main/java/com/beeline/cc_question/entities/Question.java
+++ src/main/java/com/beeline/cc_question/entities/Question.java
@@ -20,6 +20,8 @@ public class Question {
 
     private boolean hasVideo;
 
+    private Integer duration;
+
     @OneToMany(cascade = CascadeType.ALL, fetch=FetchType.LAZY)
     @JoinColumn(name="question_id")
     private List<Answer> answers;
@@ -27,15 +29,32 @@ public class Question {
     public Question(){
     }
 
-    public Question(String title, String type, boolean hasVideo) {
+    public Question(String title, String type, boolean hasVideo, int duration) {
         this.title = title;
         this.type = type;
         this.hasVideo = hasVideo;
+        this.duration = duration;
         this.answers = new ArrayList<>();
     }
 
     public void addAnswer(Answer answer){
         answers.add(answer);
+    }
+
+    @Override
+    public String toString() {
+        String result = "Question{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", type='" + type + '\'' +
+                ", hasVideo=" + hasVideo +
+                '}';
+        if (hasVideo == false)  {
+            result += ", answers=" + answers;
+        } else {
+            result += ", duration=" + duration;
+        }
+        return result;
     }
 }
 
